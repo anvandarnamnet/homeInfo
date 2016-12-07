cec19ed6252b00b4fc48e27ee78209b66e5770ab
Oskar Råhén <Oskar Rhn>
2016-12-07 15:44:53 +0100

    apisjd

--- GetCal.js
+++ GetCal.js
@@ -43,7 +43,7 @@ var authorize = function(credentials) {
     // Check if we have previously stored a token.
     fs.readFile(TOKEN_PATH, function(err, token) {
       if (err) {
-        //getNewToken(oauth2Client, callback);
+        getNewToken(oauth2Client);
       } else {
         oauth2Client.credentials = JSON.parse(token);
 
@@ -52,6 +52,47 @@ var authorize = function(credentials) {
     });
   })
 
+}
+
+function getNewToken(oauth2Client, callback) {
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
 }
 
 var getCal = function(auth){
@@ -87,10 +128,6 @@ var setupCal = function(){
       resolve(JSON.parse(content));
   });
 
-    // Authorize a client with the loaded credentials, then call the
-    // Google Calendar API.
-
-    //authorize(JSON.parse(content), listEvents);
   });
 }
 
--- index.js
+++ index.js
@@ -17,6 +17,16 @@ app.use("/getCal", getcal);
 app.use("/getSl", sl);
 app.use("/getRain", wheater);
 
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === "oskar") {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
