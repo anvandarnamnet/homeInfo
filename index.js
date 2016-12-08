06ef0df939a5a40365f781df35e60433c518b05e
Oskar Råhén <Oskar Rhn>
2016-12-08 16:51:46 +0100

    apdsj

--- index.js
+++ index.js
@@ -17,15 +17,8 @@ app.use("/getCal", getcal);
 app.use("/getSl", sl);
 app.use("/getRain", wheater);
 
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === "oskar") {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
-  }
+app.get('/', function(req, res) {
+  res.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
